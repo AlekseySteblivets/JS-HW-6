f5139e027893f2c636f81e732f12b72ef034abe4
Aleksey Steblivets <aleksey.steblivets@gmail.com>
2021-02-08 14:55:31 +0200

    сохраняет решение задачи №5

--- test.js
+++ test.js
@@ -91,15 +91,31 @@
 
 // 6-4-44Выполни рефакторинг функции calculateTotalPrice() так, чтобы она была объявлена как стрелочная.
 
-// Пиши код ниже этой строки
+// // Пиши код ниже этой строки
 
-// function calculateTotalPrice(quantity, pricePerItem) {
-//     // Пиши код выше этой строки
+// // function calculateTotalPrice(quantity, pricePerItem) {
+// //     // Пиши код выше этой строки
+// //     return quantity * pricePerItem;
+// // }
+
+// const calculateTotalPrice = (quantity, pricePerItem) => quantity * pricePerItem;
+
+// console.log(calculateTotalPrice(5, 100));
+// console.log(calculateTotalPrice(8, 60));
+// console.log(calculateTotalPrice(3, 400));
+
+
+
+// 6-5-44Выполни рефакторинг функции calculateTotalPrice() так, чтобы она была объявлена как стрелочная.
+
+// Пиши код ниже этой строки
+// const calculateTotalPrice = (quantity, pricePerItem) => {
 //     return quantity * pricePerItem;
-// }
+// };
 
 const calculateTotalPrice = (quantity, pricePerItem) => quantity * pricePerItem;
 
+// Пиши код выше этой строки
 console.log(calculateTotalPrice(5, 100));
 console.log(calculateTotalPrice(8, 60));
 console.log(calculateTotalPrice(3, 400));
