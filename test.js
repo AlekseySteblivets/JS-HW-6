d59c96d6ce186df9a4062a1ee02e0895c8089c9f
Aleksey Steblivets <aleksey.steblivets@gmail.com>
2021-02-09 08:56:48 +0200

    сохраняет решение задачи №7

--- test.js
+++ test.js
@@ -126,14 +126,47 @@
 // Замени коллбек - функцию передаваемую в метод forEach() на стрелочную функцию.
 
 // Пиши код ниже этой строки
-const calculateTotalPrice = ((orderedItems) => {
-    let totalPrice = 0;
-    orderedItems.forEach(element => {
-        totalPrice += element;
-        // console.log(totalPrice);
-    });
-    return totalPrice;
-});
+// const calculateTotalPrice = ((orderedItems) => {
+//     let totalPrice = 0;
+//     orderedItems.forEach(element => {
+//         totalPrice += element;
+//         // console.log(totalPrice);
+//     });
+//     return totalPrice;
+// });
 
-console.log(calculateTotalPrice([12, 85, 37, 4]));
-console.log(calculateTotalPrice([164, 48, 291]));
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
+// console.log(calculateTotalPrice([164, 48, 291]));
+
+
+
+// 6-7-44Замени объявление функции filterArray() и коллбек для метода forEach() на стрелочные функции.
+// Пиши код ниже этой строки
+
+const filterArray = (numbers, value) => {
+    const filteredNumbers = [];
+
+    numbers.forEach(element => {
+        if (element > value) { filteredNumbers.push(element) }
+    });
+    return filteredNumbers;
+};
+
+// Пиши код выше этой строки
+
+// function filterArray(numbers, value) {
+//     const filteredNumbers = [];
+
+//     numbers.forEach(function (number) {
+//         if (number > value) {
+//             filteredNumbers.push(number);
+//         }
+//     });
+//     return filteredNumbers;
+// }
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
