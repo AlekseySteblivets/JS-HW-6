cb3aae24e95eeb64dee492949a0b23df69b66fd2
Aleksey Steblivets <aleksey.steblivets@gmail.com>
2021-02-10 12:40:35 +0200

    сохраняет решение задачи №13

--- test.js
+++ test.js
@@ -274,24 +274,115 @@
 
 // 6-12-44Используя метод flatMap() сделай так, чтобы в переменной genres получился
 // массив всех жанров книг(свойство genres) из массива книг books.
-const books = [
+// const books = [
+//     {
+//         title: 'Последнее королевство',
+//         author: 'Бернард Корнуэлл',
+//         genres: ['приключения', 'историческое']
+//     },
+//     {
+//         title: 'На берегу спокойных вод',
+//         author: 'Роберт Шекли',
+//         genres: ['фантастика']
+//     },
+//     {
+//         title: 'Красна как кровь',
+//         author: 'Ли Танит',
+//         genres: ['ужасы', 'мистика']
+//     }
+// ];
+// // Пиши код ниже этой строки
+
+// const genres = books.flatMap(book => book.genres);
+// console.log(genres);
+
+
+
+// 6-13-44Дополни функцию getUserNames(users) так, чтобы она возвращала массив имён пользователей (свойство name)
+// из массива объектов в параметре users.
+
+const users = [
     {
-        title: 'Последнее королевство',
-        author: 'Бернард Корнуэлл',
-        genres: ['приключения', 'историческое']
+        name: 'Moore Hensley',
+        email: 'moorehensley@indexia.com',
+        eyeColor: 'blue',
+        friends: ['Sharron Pace'],
+        isActive: false,
+        balance: 2811,
+        skills: ['ipsum', 'lorem'],
+        gender: 'male',
+        age: 37,
     },
     {
-        title: 'На берегу спокойных вод',
-        author: 'Роберт Шекли',
-        genres: ['фантастика']
+        name: 'Sharlene Bush',
+        email: 'sharlenebush@tubesys.com',
+        eyeColor: 'blue',
+        friends: ['Briana Decker', 'Sharron Pace'],
+        isActive: true,
+        balance: 3821,
+        skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
+        gender: 'female',
+        age: 34,
     },
     {
-        title: 'Красна как кровь',
-        author: 'Ли Танит',
-        genres: ['ужасы', 'мистика']
-    }
+        name: 'Ross Vazquez',
+        email: 'rossvazquez@xinware.com',
+        eyeColor: 'green',
+        friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+        isActive: false,
+        balance: 3793,
+        skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+        gender: 'male',
+        age: 24,
+    },
+    {
+        name: 'Elma Head',
+        email: 'elmahead@omatom.com',
+        eyeColor: 'green',
+        friends: ['Goldie Gentry', 'Aisha Tran'],
+        isActive: true,
+        balance: 2278,
+        skills: ['adipisicing', 'irure', 'velit'],
+        gender: 'female',
+        age: 21,
+    },
+    {
+        name: 'Carey Barr',
+        email: 'careybarr@nurali.com',
+        eyeColor: 'blue',
+        friends: ['Jordan Sampson', 'Eddie Strong'],
+        isActive: true,
+        balance: 3951,
+        skills: ['ex', 'culpa', 'nostrud'],
+        gender: 'male',
+        age: 27,
+    },
+    {
+        name: 'Blackburn Dotson',
+        email: 'blackburndotson@furnigeer.com',
+        eyeColor: 'brown',
+        friends: ['Jacklyn Lucas', 'Linda Chapman'],
+        isActive: false,
+        balance: 1498,
+        skills: ['non', 'amet', 'ipsum'],
+        gender: 'male',
+        age: 38,
+    },
+    {
+        name: 'Sheree Anthony',
+        email: 'shereeanthony@kog.com',
+        eyeColor: 'brown',
+        friends: ['Goldie Gentry', 'Briana Decker'],
+        isActive: true,
+        balance: 2764,
+        skills: ['lorem', 'veniam', 'culpa'],
+        gender: 'female',
+        age: 39,
+    },
 ];
 // Пиши код ниже этой строки
+// const getUserNames = users.map(user => user.name);
+const getUserNames = users => users.map(user => user.name);
+// Пиши код выше этой строки
 
-const genres = books.flatMap(book => book.genres);
-console.log(genres);
+console.log(getUserNames(users));
