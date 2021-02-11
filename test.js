c57b47b3bca5e6c326f4cb919b93ec2b4b7c07b2
Aleksey Steblivets <aleksey.steblivets@gmail.com>
2021-02-11 14:03:51 +0200

    сохраняет решение задачи №16

--- test.js
+++ test.js
@@ -426,10 +426,39 @@
 // 6-15-44Дополни код так, чтобы в переменной evenNumbers получился массив чётных чисел из массива numbers, 
 // а в переменной oddNumbers массив нечётных.Обязательно используй метод filter().
 
-const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
-// Пиши код ниже этой строки
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// // Пиши код ниже этой строки
 
-const evenNumbers = numbers.filter(number => number % 2 === 0);
-const oddNumbers = numbers.filter(number => number % 2 !== 0);
-console.log(evenNumbers);
-console.log(oddNumbers);
+// const evenNumbers = numbers.filter(number => number % 2 === 0);
+// const oddNumbers = numbers.filter(number => number % 2 !== 0);
+// console.log(evenNumbers);
+// console.log(oddNumbers);
+
+
+
+// 6-16-44Дополни код так, чтобы в переменной allGenres был массив всех жанров книг (свойство genres) из массива books, 
+// а в переменной uniqueGenres массив уникальных жанров - без повторений.
+
+const books = [
+    {
+        title: 'Последнее королевство',
+        author: 'Бернард Корнуэлл',
+        genres: ['приключения', 'историческое']
+    },
+    {
+        title: 'На берегу спокойных вод',
+        author: 'Роберт Шекли',
+        genres: ['фантастика', 'мистика']
+    },
+    {
+        title: 'Красна как кровь',
+        author: 'Ли Танит',
+        genres: ['ужасы', 'мистика', 'приключения']
+    }
+];
+// Пиши код ниже этой строки
+const allGenres = books.flatMap(book => book.genres);
+const uniqueGenres = allGenres.filter((book, index, array) => array.indexOf(book) === index);
+
+console.log(allGenres);
+console.log(uniqueGenres);
