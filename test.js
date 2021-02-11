685123db0c8eaa5049c0c543a672a4030595dcd6
Aleksey Steblivets <aleksey.steblivets@gmail.com>
2021-02-11 15:13:53 +0200

    сохраняет решение задачи №20

--- test.js
+++ test.js
@@ -494,11 +494,167 @@
 // 6-18-44Дополни функцию getUsersWithEyeColor(users, color) так, чтобы она возвращала массив пользователей у 
 // которых цвет глаз(свойство eyeColor) совпадает со значением параметра color.
 
-// Пиши код ниже этой строки
-const getUsersWithEyeColor = (users, color) => users.filter(user => user.eyeColor === color);
+// // Пиши код ниже этой строки
+// const getUsersWithEyeColor = (users, color) => users.filter(user => user.eyeColor === color);
 
-// Пиши код выше этой строки
-console.log(getUsersWithEyeColor([
+// // Пиши код выше этой строки
+// console.log(getUsersWithEyeColor([
+//     {
+//         name: 'Moore Hensley',
+//         email: 'moorehensley@indexia.com',
+//         eyeColor: 'blue',
+//         friends: ['Sharron Pace'],
+//         isActive: false,
+//         balance: 2811,
+//         gender: 'male'
+//     },
+//     {
+//         name: 'Sharlene Bush',
+//         email: 'sharlenebush@tubesys.com',
+//         eyeColor: 'blue',
+//         friends: ['Briana Decker', 'Sharron Pace'],
+//         isActive: true,
+//         balance: 3821,
+//         gender: 'female'
+//     },
+//     {
+//         name: 'Ross Vazquez',
+//         email: 'rossvazquez@xinware.com',
+//         eyeColor: 'green',
+//         friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//         isActive: false,
+//         balance: 3793,
+//         gender: 'male'
+//     },
+//     {
+//         name: 'Elma Head',
+//         email: 'elmahead@omatom.com',
+//         eyeColor: 'green',
+//         friends: ['Goldie Gentry', 'Aisha Tran'],
+//         isActive: true,
+//         balance: 2278,
+//         gender: 'female'
+//     },
+//     {
+//         name: 'Carey Barr',
+//         email: 'careybarr@nurali.com',
+//         eyeColor: 'blue',
+//         friends: ['Jordan Sampson', 'Eddie Strong'],
+//         isActive: true,
+//         balance: 3951,
+//         gender: 'male'
+//     },
+//     {
+//         name: 'Blackburn Dotson',
+//         email: 'blackburndotson@furnigeer.com',
+//         eyeColor: 'brown',
+//         friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//         isActive: false,
+//         balance: 1498,
+//         gender: 'male'
+//     },
+//     {
+//         name: 'Sheree Anthony',
+//         email: 'shereeanthony@kog.com',
+//         eyeColor: 'brown',
+//         friends: ['Goldie Gentry', 'Briana Decker'],
+//         isActive: true,
+//         balance: 2764,
+//         gender: 'female'
+//     }
+// ], "blue"));
+
+
+
+// 6-19-44Дополни функцию getUsersWithAge(users, minAge, maxAge) так, чтобы она возвращала массив пользователей, 
+// возраст которых(свойство age) попадает в промежуток от minAge до maxAge.
+
+// // Пиши код ниже этой строки
+// const users = [
+//     {
+//         name: 'Moore Hensley',
+//         email: 'moorehensley@indexia.com',
+//         eyeColor: 'blue',
+//         friends: ['Sharron Pace'],
+//         isActive: false,
+//         balance: 2811,
+//         gender: 'male',
+//         age: 37
+//     },
+//     {
+//         name: 'Sharlene Bush',
+//         email: 'sharlenebush@tubesys.com',
+//         eyeColor: 'blue',
+//         friends: ['Briana Decker', 'Sharron Pace'],
+//         isActive: true,
+//         balance: 3821,
+//         gender: 'female',
+//         age: 34
+//     },
+//     {
+//         name: 'Ross Vazquez',
+//         email: 'rossvazquez@xinware.com',
+//         eyeColor: 'green',
+//         friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//         isActive: false,
+//         balance: 3793,
+//         gender: 'male',
+//         age: 24
+//     },
+//     {
+//         name: 'Elma Head',
+//         email: 'elmahead@omatom.com',
+//         eyeColor: 'green',
+//         friends: ['Goldie Gentry', 'Aisha Tran'],
+//         isActive: true,
+//         balance: 2278,
+//         gender: 'female',
+//         age: 21
+//     },
+//     {
+//         name: 'Carey Barr',
+//         email: 'careybarr@nurali.com',
+//         eyeColor: 'blue',
+//         friends: ['Jordan Sampson', 'Eddie Strong'],
+//         isActive: true,
+//         balance: 3951,
+//         gender: 'male',
+//         age: 27
+//     },
+//     {
+//         name: 'Blackburn Dotson',
+//         email: 'blackburndotson@furnigeer.com',
+//         eyeColor: 'brown',
+//         friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//         isActive: false,
+//         balance: 1498,
+//         gender: 'male',
+//         age: 38
+//     },
+//     {
+//         name: 'Sheree Anthony',
+//         email: 'shereeanthony@kog.com',
+//         eyeColor: 'brown',
+//         friends: ['Goldie Gentry', 'Briana Decker'],
+//         isActive: true,
+//         balance: 2764,
+//         gender: 'female',
+//         age: 39
+//     }
+// ];
+// const getUsersWithAge = (users, minAge, maxAge) => users.filter(user => user.age > minAge && user.age < maxAge);
+
+// // Пиши код выше этой строки
+
+// console.log(getUsersWithAge(users, 20, 30));
+
+
+
+// 6-20-44Дополни функцию getUsersWithFriend(users, friendName) так, чтобы она возвращала массив пользователей 
+// у которых есть друг с именем в параметре friendName.Массив друзей пользователя хранится в свойстве friends.
+
+// Пиши код ниже этой строки
+const users = [
     {
         name: 'Moore Hensley',
         email: 'moorehensley@indexia.com',
@@ -506,7 +662,8 @@ console.log(getUsersWithEyeColor([
         friends: ['Sharron Pace'],
         isActive: false,
         balance: 2811,
-        gender: 'male'
+        gender: 'male',
+        age: 37
     },
     {
         name: 'Sharlene Bush',
@@ -515,7 +672,8 @@ console.log(getUsersWithEyeColor([
         friends: ['Briana Decker', 'Sharron Pace'],
         isActive: true,
         balance: 3821,
-        gender: 'female'
+        gender: 'female',
+        age: 34
     },
     {
         name: 'Ross Vazquez',
@@ -524,7 +682,8 @@ console.log(getUsersWithEyeColor([
         friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
         isActive: false,
         balance: 3793,
-        gender: 'male'
+        gender: 'male',
+        age: 24
     },
     {
         name: 'Elma Head',
@@ -533,7 +692,8 @@ console.log(getUsersWithEyeColor([
         friends: ['Goldie Gentry', 'Aisha Tran'],
         isActive: true,
         balance: 2278,
-        gender: 'female'
+        gender: 'female',
+        age: 21
     },
     {
         name: 'Carey Barr',
@@ -542,7 +702,8 @@ console.log(getUsersWithEyeColor([
         friends: ['Jordan Sampson', 'Eddie Strong'],
         isActive: true,
         balance: 3951,
-        gender: 'male'
+        gender: 'male',
+        age: 27
     },
     {
         name: 'Blackburn Dotson',
@@ -551,7 +712,8 @@ console.log(getUsersWithEyeColor([
         friends: ['Jacklyn Lucas', 'Linda Chapman'],
         isActive: false,
         balance: 1498,
-        gender: 'male'
+        gender: 'male',
+        age: 38
     },
     {
         name: 'Sheree Anthony',
@@ -560,6 +722,12 @@ console.log(getUsersWithEyeColor([
         friends: ['Goldie Gentry', 'Briana Decker'],
         isActive: true,
         balance: 2764,
-        gender: 'female'
+        gender: 'female',
+        age: 39
     }
-], "blue"));
+];
+const getUsersWithFriend = (users, friendName) => users.filter(user => user.friends.includes(friendName));
+
+// Пиши код выше этой строки
+
+console.log(getUsersWithFriend(users, 'Briana Decker'));
