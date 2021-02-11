4a6ded346a1ce878129654a98e27d35202861e0e
Aleksey Steblivets <aleksey.steblivets@gmail.com>
2021-02-11 14:34:02 +0200

    сохраняет решение задачи №18

--- test.js
+++ test.js
@@ -471,20 +471,95 @@
 // В переменной booksByAuthor получился массив книг написанных автором с именем(свойство author)
 // которое совпадает со значением в переменной AUTHOR.
 
-const books = [
-    { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
-    { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
-    { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
-    { title: 'Красна как кровь', author: 'Ли Танит', rating: 7.94 },
-    { title: 'Враг Божий', author: 'Бернард Корнуэлл', rating: 8.67 }
-];
+// const books = [
+//     { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
+//     { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
+//     { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
+//     { title: 'Красна как кровь', author: 'Ли Танит', rating: 7.94 },
+//     { title: 'Враг Божий', author: 'Бернард Корнуэлл', rating: 8.67 }
+// ];
 
-const MIN_RATING = 8;
-const AUTHOR = 'Бернард Корнуэлл';
+// const MIN_RATING = 8;
+// const AUTHOR = 'Бернард Корнуэлл';
+// // Пиши код ниже этой строки
+
+// const topRatedBooks = books.filter(book => book.rating >= MIN_RATING);
+// const booksByAuthor = books.filter(book => book.author === AUTHOR);
+
+// console.log(topRatedBooks);
+// console.log(booksByAuthor);
+
+
+
+// 6-18-44Дополни функцию getUsersWithEyeColor(users, color) так, чтобы она возвращала массив пользователей у 
+// которых цвет глаз(свойство eyeColor) совпадает со значением параметра color.
+
 // Пиши код ниже этой строки
+const getUsersWithEyeColor = (users, color) => users.filter(user => user.eyeColor === color);
 
-const topRatedBooks = books.filter(book => book.rating >= MIN_RATING);
-const booksByAuthor = books.filter(book => book.author === AUTHOR);
-
-console.log(topRatedBooks);
-console.log(booksByAuthor);
+// Пиши код выше этой строки
+console.log(getUsersWithEyeColor([
+    {
+        name: 'Moore Hensley',
+        email: 'moorehensley@indexia.com',
+        eyeColor: 'blue',
+        friends: ['Sharron Pace'],
+        isActive: false,
+        balance: 2811,
+        gender: 'male'
+    },
+    {
+        name: 'Sharlene Bush',
+        email: 'sharlenebush@tubesys.com',
+        eyeColor: 'blue',
+        friends: ['Briana Decker', 'Sharron Pace'],
+        isActive: true,
+        balance: 3821,
+        gender: 'female'
+    },
+    {
+        name: 'Ross Vazquez',
+        email: 'rossvazquez@xinware.com',
+        eyeColor: 'green',
+        friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+        isActive: false,
+        balance: 3793,
+        gender: 'male'
+    },
+    {
+        name: 'Elma Head',
+        email: 'elmahead@omatom.com',
+        eyeColor: 'green',
+        friends: ['Goldie Gentry', 'Aisha Tran'],
+        isActive: true,
+        balance: 2278,
+        gender: 'female'
+    },
+    {
+        name: 'Carey Barr',
+        email: 'careybarr@nurali.com',
+        eyeColor: 'blue',
+        friends: ['Jordan Sampson', 'Eddie Strong'],
+        isActive: true,
+        balance: 3951,
+        gender: 'male'
+    },
+    {
+        name: 'Blackburn Dotson',
+        email: 'blackburndotson@furnigeer.com',
+        eyeColor: 'brown',
+        friends: ['Jacklyn Lucas', 'Linda Chapman'],
+        isActive: false,
+        balance: 1498,
+        gender: 'male'
+    },
+    {
+        name: 'Sheree Anthony',
+        email: 'shereeanthony@kog.com',
+        eyeColor: 'brown',
+        friends: ['Goldie Gentry', 'Briana Decker'],
+        isActive: true,
+        balance: 2764,
+        gender: 'female'
+    }
+], "blue"));
