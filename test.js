2045820c1fee9d4c154dda9bd480436cd504d9ec
Aleksey Steblivets <aleksey.steblivets@gmail.com>
2021-02-14 20:58:13 +0200

    сохраняет решение задачи №35

--- test.js
+++ test.js
@@ -1411,18 +1411,33 @@ const users = [
 // возрастанию копия массива releaseDates,
 // а в переменной alphabeticalAuthors копия массива имён авторов authors отсортированная в по алфавиту.
 
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// const authors = [
+//     'Ли Танит',
+//     'Бернард Корнуэлл',
+//     'Роберт Шекли',
+//     'Федор Достоевский'
+// ];
+// // Пиши код ниже этой строки
+
+// const ascendingReleaseDates = [...releaseDates].sort();
+
+// const alphabeticalAuthors = [...authors].sort();
+
+// console.log(ascendingReleaseDates);
+// console.log(alphabeticalAuthors);
+
+
+// 6-35-44Онлайн бибилиотеке необходимо отображать книги сортированные по дате издания, 
+// по её возрастанию или убыванию.Дополни код так, чтобы в переменной ascendingReleaseDates получилась
+// отсортированная по возрастанию копия массива releaseDates, а в переменной descendingReleaseDates копия отсортированная по убыванию.
+
 const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
-const authors = [
-    'Ли Танит',
-    'Бернард Корнуэлл',
-    'Роберт Шекли',
-    'Федор Достоевский'
-];
 // Пиши код ниже этой строки
 
 const ascendingReleaseDates = [...releaseDates].sort();
 
-const alphabeticalAuthors = [...authors].sort();
+const descendingReleaseDates = [...releaseDates].sort((a, b) => b - a);
 
 console.log(ascendingReleaseDates);
-console.log(alphabeticalAuthors);
+console.log(descendingReleaseDates);
