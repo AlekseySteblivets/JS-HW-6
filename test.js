08ba4141da7d287ea7607adada243908576e74c8
Aleksey Steblivets <aleksey.steblivets@gmail.com>
2021-02-14 23:42:04 +0200

    сохраняет решение задачи №41

--- test.js
+++ test.js
@@ -1521,8 +1521,27 @@ const users = [
 // 6-40-44Дополни функцию sortByName(users) так, чтобы она возвращала массив пользователей отсортированный
 // по их имени(свойство name) в алфавитном порядке.
 
-// Пиши код ниже этой строки
-const sortByName = users => [...users].sort((a, b) => a.name.localeCompare(b.name));
+// // Пиши код ниже этой строки
+// const sortByName = users => [...users].sort((a, b) => a.name.localeCompare(b.name));
 
-// Пиши код выше этой строки
-console.log(sortByName(users));
+// // Пиши код выше этой строки
+// console.log(sortByName(users));
+
+
+
+// 6-41-44Дополни код так, чтобы в переменной names получился массив имён авторов в алфавитном порядке, 
+// рейтинг книг которых больше значения переменной MIN_BOOK_RATING.
+
+const books = [
+    { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
+    { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
+    { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
+    { title: 'Красна как кровь', author: 'Ли Танит', rating: 8.14 },
+    { title: 'Сны В Ведьмином Доме', author: 'Говард Лавкрафт', rating: 8.67 }
+];
+const MIN_BOOK_RATING = 8;
+// Пиши код ниже этой строки
+
+const names = books.filter(book => book.rating > MIN_BOOK_RATING).map(book => book.author).sort((a, b) => a.localeCompare(b));
+// .map(book => book.author);
+console.log(names);
