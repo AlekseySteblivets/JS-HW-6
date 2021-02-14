74af3a6eb938927292f682158d2904909275cc87
Aleksey Steblivets <aleksey.steblivets@gmail.com>
2021-02-14 19:22:34 +0200

    сохраняет решение задачи №32

--- test.js
+++ test.js
@@ -1303,17 +1303,92 @@
 // и получить общую сумму этих времён.Рассчитать время для каждого из игроков, можно разделив его
 // время(свойство playtime) на количество игр(свойство gamesPlayed).
 
-const players = [
-    { name: 'Манго', playtime: 1270, gamesPlayed: 4 },
-    { name: 'Поли', playtime: 469, gamesPlayed: 2 },
-    { name: 'Аякс', playtime: 690, gamesPlayed: 3 },
-    { name: 'Киви', playtime: 241, gamesPlayed: 1 },
-];
+// const players = [
+//     { name: 'Манго', playtime: 1270, gamesPlayed: 4 },
+//     { name: 'Поли', playtime: 469, gamesPlayed: 2 },
+//     { name: 'Аякс', playtime: 690, gamesPlayed: 3 },
+//     { name: 'Киви', playtime: 241, gamesPlayed: 1 },
+// ];
+// // Пиши код ниже этой строки
+
+// const totalAveragePlaytimePerGame = players.reduce((total, player) => {
+
+//     return total + player.playtime / player.gamesPlayed
+
+// }, 0);
+// console.log(totalAveragePlaytimePerGame);
+
+
+
+// 6-32-44Дополни функцию calculateTotalBalance(users) так, чтобы она считала и возвращала сумму всех средств 
+// (свойство balance) которые хранят пользователи из массива users.
 // Пиши код ниже этой строки
+// const users = [
+//     {
+//         name: 'Moore Hensley',
+//         email: 'moorehensley@indexia.com',
+//         eyeColor: 'blue',
+//         friends: ['Sharron Pace'],
+//         isActive: false,
+//         balance: 2811,
+//         gender: 'male'
+//     },
+//     {
+//         name: 'Sharlene Bush',
+//         email: 'sharlenebush@tubesys.com',
+//         eyeColor: 'blue',
+//         friends: ['Briana Decker', 'Sharron Pace'],
+//         isActive: true,
+//         balance: 3821,
+//         gender: 'female'
+//     },
+//     {
+//         name: 'Ross Vazquez',
+//         email: 'rossvazquez@xinware.com',
+//         eyeColor: 'green',
+//         friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//         isActive: false,
+//         balance: 3793,
+//         gender: 'male'
+//     },
+//     {
+//         name: 'Elma Head',
+//         email: 'elmahead@omatom.com',
+//         eyeColor: 'green',
+//         friends: ['Goldie Gentry', 'Aisha Tran'],
+//         isActive: true,
+//         balance: 2278,
+//         gender: 'female'
+//     },
+//     {
+//         name: 'Carey Barr',
+//         email: 'careybarr@nurali.com',
+//         eyeColor: 'blue',
+//         friends: ['Jordan Sampson', 'Eddie Strong'],
+//         isActive: true,
+//         balance: 3951,
+//         gender: 'male'
+//     },
+//     {
+//         name: 'Blackburn Dotson',
+//         email: 'blackburndotson@furnigeer.com',
+//         eyeColor: 'brown',
+//         friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//         isActive: false,
+//         balance: 1498,
+//         gender: 'male'
+//     },
+//     {
+//         name: 'Sheree Anthony',
+//         email: 'shereeanthony@kog.com',
+//         eyeColor: 'brown',
+//         friends: ['Goldie Gentry', 'Briana Decker'],
+//         isActive: true,
+//         balance: 2764,
+//         gender: 'female'
+//     }
+// ];
+// const calculateTotalBalance = users => users.reduce((total, user) => { return total + user.balance }, 0);
 
-const totalAveragePlaytimePerGame = players.reduce((total, player) => {
-
-    return total + player.playtime / player.gamesPlayed
-
-}, 0);
-console.log(totalAveragePlaytimePerGame);
+// // Пиши код выше этой строки
+// console.log(calculateTotalBalance(users));
